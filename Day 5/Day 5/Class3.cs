3441447512ec6655df20f5a47b9c7605197ca426
Madhumitha-2002 <madhumithab.2512@gmail.com>
2022-09-13 11:05:15 +0530

    Day 5

---
+++ Day 5/Day 5/Class3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_5
+{
+    abstract class Animal
+    {
+        //hidden implementing abstraction
+        string Name;
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+        abstract public void Eat();
+    }
+    class Dog : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Dog is eating");
+        }
+    }
+    class Class3
+    {
+        public static void Main()
+        {
+            Dog d = new Dog();
+            d.SetName("Dummy");
+            Console.WriteLine(d.GetName());
+            d.Eat();
+        }
+    }
+}
+
---
+++ Day 5/Day 5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Day_5
+{
+    class Program
+    {
+        abstract class abc
+        {
+            abstract public void print();
+            public void show()
+            {
+                Console.WriteLine("asdasd");
+            }
+        }
+        class ABC2 : abc
+        {
+            int a = 5;
+            int b = 5;
+            public override void print()
+            {
+                Console.WriteLine("I am print Method");
+            }
+            public override string ToString()
+            {
+                return "a= " + a + "b= " + b;
+            }
+
+        }
+        class Class1
+        {
+            public static void Main1()
+            {
+                ABC2 abc = new ABC2();
+                string sampl = abc.ToString();
+                Console.WriteLine(sampl);
+            }
+        }
+    }
+}
+  
