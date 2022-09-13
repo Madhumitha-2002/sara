5ead8d777cec69790c0053c3a0df2ca3e42feb34
Madhumitha-2002 <madhumithab.2512@gmail.com>
2022-09-13 11:01:08 +0530

    day10

---
+++ day10/day10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace day10
+{
+    class Class1
+    {
+        static int Sum(int x, int y)
+        {
+            return (x + y);
+        }
+        static void Print(int i)
+        {
+            Console.WriteLine(i);
+        }
+        static bool IsPostiveNumber(int val)
+        {
+            if (val > 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public static void Main()
+        {
+            Func<int, int, int> add = Sum;
+            int result = add(10, 10);
+            Console.WriteLine(result);
+            Action<int> printVal = Print;
+            printVal(10);
+            Predicate<int> predicate = IsPostiveNumber;
+            Console.WriteLine(predicate(1));
+        }
+    }
+}
+
+
